c8860a86a700bb04a678e8038fa59a43fc56ea86
mushtaqshahzad <mushtaqshahzad49@gmail.com>
2023-07-21 12:13:50 +0500

    project completed

---
+++ components/index.js
@@ -0,0 +1,18 @@
+import HomeHeader from './HomeHeader';
+import FocusedStatusBar from './FocusedStatusBar';
+import NFTCard from './NFTCard';
+import DetailsDesc from './DetailsDesc';
+import DetailsBid from './DetailsBid';
+import { CircleButton, RectButton } from './Button';
+import { SubInfo } from './SubInfo';
+
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  SubInfo,
+  DetailsDesc,
+  DetailsBid,
+};
